7905891155d5d05fd8b7ae3b5b75e7ca994476a4
Kelvin <kelvin.huberts@capgemini.com>
2017-10-30 10:49:25 +0100

    Changes to program

--- SuperFancyPants/Program.cs
+++ SuperFancyPants/Program.cs
@@ -7,6 +7,19 @@ namespace SuperFancyPants
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Print();
+
+            Console.ReadKey();
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Vanuit de print methode");
+
+            string result = Console.ReadLine();
+
+            Console.WriteLine($"Dit was het resultaat: {result}");
         }
     }
 }
